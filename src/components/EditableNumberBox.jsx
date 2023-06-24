ad22bc9126e7a6bebadbe40a18091bf4f0328cf7
Traison <trais.diedrich@gmail.com>
2023-06-24 00:01:36 -0500

    better numberbox formatting, separated editable and normal numberbox, stepper and textfield work together to edit same state, main container fills entire viewport

---
+++ src/components/EditableNumberBox.jsx
@@ -0,0 +1,100 @@
+import { Box, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import NumberStepper from './NumberStepper';
+
+/**
+ * For most use cases, min should be set to 0. min = 1 will
+ * prevent user from backspacing their input to less than 1 digit
+ */
+const EditableNumberBox = ({
+	title,
+	number,
+	setNumber,
+	onEdit,
+	min,
+	max,
+	step,
+}) => {
+	const [error, setError] = useState(false);
+
+	const getNumDigits = (num) => {
+		return Math.max(Math.floor(Math.log10(Math.abs(num))) + 1, 1);
+	};
+
+	const [numDigits, setNumDigits] = useState(getNumDigits(number));
+
+	const handleEdit = (e) => {
+		const { value } = e.target;
+		const parsedValue = Number(value);
+
+		console.log(value, parsedValue, number);
+
+		validateNumber(parsedValue);
+	};
+
+	const validateNumber = (num) => {
+		if (!isNaN(num) && num >= min && num <= max) {
+			setError(false);
+			setNumDigits(getNumDigits(num));
+			onEdit(num, setNumber);
+		} else {
+			setError(true);
+		}
+	};
+
+	return (
+		<Box
+			display='flex'
+			flexDirection='column'
+			justifyContent='center'
+			alignItems='center'>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					border: '2px solid',
+					justifyContent: 'center',
+					alignContent: 'center',
+					alignItems: 'center',
+					borderColor: 'primary.main',
+					borderRadius: '3px',
+					px: 1,
+					mx: 1,
+				}}>
+				<TextField
+					value={number}
+					variant='standard'
+					size='small'
+					onChange={handleEdit}
+					error={error}
+					// min+1 here to accommodate backspacing
+					helperText={error ? `${min + 1}-${max}` : ''}
+					sx={{
+						'& input': {
+							fontSize: '3.0rem',
+							textAlign: 'center',
+							padding: '0px',
+							margin: '0px',
+						},
+						'& .MuiFormHelperText-root': {
+							textAlign: 'center',
+						},
+						'& .MuiInputBase-root': {
+							width: `${32 * numDigits}px`,
+							height: '64px',
+						},
+					}}
+				/>
+				<Typography variant='caption' align='center' sx={{ py: 0.5 }}>
+					{title}
+				</Typography>
+			</Box>
+			<NumberStepper
+				increment={() => validateNumber(number + step)}
+				decrement={() => validateNumber(number - step)}
+			/>
+		</Box>
+	);
+};
+
+export default EditableNumberBox;
--- src/components/NumberBox.jsx
+++ src/components/NumberBox.jsx
@@ -1,69 +1,25 @@
-import { Box, Divider, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Box, Divider, Typography } from '@mui/material';
 
-/**
- * TODO: More functionality being handled than I would like by the number box.
- * Separate into read only number box and an editableNumber box and then combine
- * error handling into editable section.
- */
-
-const NumberBox = ({ title, number, setNumber, editing, onEdit, min, max }) => {
-	const [error, setError] = useState(false);
-
-	const handleEdit = (e) => {
-		const { value } = e.target;
-		const parsedValue = Number(value);
-
-		if (!isNaN(parsedValue) && parsedValue >= min && parsedValue <= max) {
-			setError(false);
-			onEdit(parsedValue, setNumber);
-		} else {
-			setError(true);
-			onEdit(1, setNumber);
-		}
-	};
-
+const NumberBox = ({ title, number }) => {
 	return (
 		<Box
 			sx={{
 				display: 'flex',
 				flexDirection: 'column',
-				alignItems: 'center',
-				justifyContent: 'center',
 				border: '2px solid',
+				justifyContent: 'center',
+				alignContent: 'center',
+				alignItems: 'center',
 				borderColor: 'primary.main',
 				borderRadius: '3px',
-				p: 1,
-				mx: 2,
-				minWidth: '107px',
-				maxWidth: '164px',
+				px: 1,
+				mx: 1,
 			}}>
-			{editing ? (
-				<TextField
-					value={number}
-					variant='standard'
-					onChange={handleEdit}
-					error={error}
-					helperText={error ? `Must be ${min}-${max}` : ''}
-					sx={{
-						p: -8,
-						'& input': {
-							fontSize: '3.0rem',
-							textAlign: 'center',
-						},
-						'& .MuiFormHelperText-root': {
-							textAlign: 'center',
-						},
-					}}
-				/>
-			) : (
-				<Typography variant='h3' align='center'>
-					{number}
-				</Typography>
-			)}
-			{/* FIXME: Divider is not rendering */}
-			{!editing && <Divider />}
-			<Typography variant='caption' align='center' sx={{ pt: 1 }}>
+			<Typography variant='h3' align='center'>
+				{number}
+			</Typography>
+			<Divider style={{ width: '80%' }} />
+			<Typography variant='caption' align='center' sx={{ py: 0.5 }}>
 				{title}
 			</Typography>
 		</Box>
