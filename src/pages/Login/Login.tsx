59b7b3e624ebc518f661efc345a46b81a8053f2e
Traison <trais.diedrich@gmail.com>
2023-08-23 14:35:44 -0500

    background image breakpoints background gradient

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import { IconAlertTriangle, IconBrandGithubFilled } from '@tabler/icons-react';
 import * as React from 'react';
 import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
+import gym from '../../assets/gym.jpg';
 import useAuth from '../../context/AuthContext';
 import { PasswordReset } from './PasswordReset';
 import WP from '/WP.svg';
@@ -38,22 +39,32 @@ export const Login: React.FC = () => {
     return session?.session ? (
         <Navigate to={state?.path || '/auth/home'} replace />
     ) : (
-        <>
+        <div className="relative flex h-screen w-full">
             <PasswordReset open={showReset} toggleOpen={toggleReset} />
-            <div className="relative grid min-h-screen w-full place-items-center">
-                <Link
-                    to="/signup"
-                    className="btn btn-ghost absolute right-8 top-8"
-                >
-                    Sign-Up
-                </Link>
-                <Link to="/" className="btn btn-ghost absolute left-8 top-8">
-                    <img
-                        src={WP}
-                        alt="Workout Planner Logo"
-                        className="h-10 w-10"
-                    />
-                </Link>
+            <Link to="/signup" className="btn btn-ghost absolute right-8 top-8">
+                Sign-Up
+            </Link>
+            <Link
+                to="/"
+                className="btn btn-ghost absolute left-8 top-8 z-10 flex items-center gap-2 lg:btn-neutral"
+            >
+                <img src={WP} alt="Workout Planner Logo" className="h-8 w-8" />
+                WP
+            </Link>
+            <div className="relative hidden h-full w-1/2 lg:block">
+                <img
+                    src={gym}
+                    alt="gym"
+                    className="h-full w-full object-cover"
+                />
+                <div className="absolute left-0 top-0 z-10 flex h-full w-full flex-col justify-end bg-opacity-20 bg-gradient-to-t from-black via-transparent to-transparent p-8">
+                    <p className="mb-2 text-lg font-bold">
+                        "Don't stop when you're tired. Stop when you're done."
+                    </p>
+                    <p className="text-sm">David Goggins</p>
+                </div>
+            </div>
+            <div className="grid min-h-screen w-full place-items-center lg:w-1/2">
                 <div className="flex h-full w-full max-w-md flex-col items-center justify-center gap-4">
                     <div className="text-center">
                         <h1 className="text-3xl font-bold">
@@ -119,6 +130,6 @@ export const Login: React.FC = () => {
                     </button>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
