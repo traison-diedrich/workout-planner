22d9dea197173c321bd11d1c3314dccfa311c3b2
Traison <trais.diedrich@gmail.com>
2023-10-17 15:47:48 -0500

    import models properly

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -7,6 +7,10 @@ from alembic import context
 
 from sqlmodel import SQLModel
 
+
+# all models must be imported here
+from app.database.models import ExerciseInfo, Exercise, Workout
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
