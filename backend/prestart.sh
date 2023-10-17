d880f23c7c940eb903468bcbe9b167b3900cce87
Traison <trais.diedrich@gmail.com>
2023-10-17 17:21:17 -0500

    init migration prestart remove autogenerate remove migrations volume

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,6 +1,5 @@
 # Let the connection to db be established
 sleep 10;
 
-# Run migrations
-python -m alembic revision --autogenerate -m "init session"
+# Run migrations (Migrations must be made locally before building the image)
 python -m alembic upgrade head
